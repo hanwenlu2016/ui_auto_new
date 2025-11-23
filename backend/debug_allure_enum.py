167f1e097db13894c52628eed10a2256d21124ea
hanwenlu2016 <370389480@qq.com>
2025-11-23 21:27:47 +0800

    删除冗余代码

--- backend/debug_allure_enum.py
+++
@@ -1,9 +0,0 @@
-from allure_commons.types import AttachmentType
-import json
-
-def debug_enum():
-    print(f"AttachmentType.PNG value: {AttachmentType.PNG.value}")
-    print(f"Type of value: {type(AttachmentType.PNG.value)}")
-
-if __name__ == "__main__":
-    debug_enum()
